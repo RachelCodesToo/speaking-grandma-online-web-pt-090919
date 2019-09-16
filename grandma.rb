4ea2bdb0bece10c3b08772fc6f480f6922ea070d
Anne Fausto <a.r.fausto@gmail.com>
2019-09-16 01:45:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def speak_to_grandma(string)
  if string == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
@@ -8,3 +9,38 @@ def speak_to_grandma(string)
  end
 end
 end
+=======
+# Write a speak_to_grandma method.
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else if string == "WHAT DID YOU EAT TODAY?"
+    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
+  else if string == string.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
+  else if string == string.upcase
+    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
+  else 
+    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+ end
+end
+end
+
+# Whatever you say to grandma, she should respond with
+# HUH?! SPEAK UP, SONNY!
+# unless you shout it (type in all capitals).
+
+# If you shout, she can hear you (or at least she thinks so) 
+# and yells back
+
+# NO, NOT SINCE !
+
+# However if you say 'I LOVE YOU GRANDMA!', she should respond with
+# 'I LOVE YOU TOO PUMPKIN!'
+>>>>>>> 774d7f68741eb05d18a8da637b8d6ac830a25c0b
